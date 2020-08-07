46112377b2003422c90969021cd5ad5f6b95aa18
Rowan Thorley <rghthorley@gmail.com>
2020-08-08 11:32:50 +1200

    create room btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+var form = document.getElementById("myForm");
+
+var buttonIndex;
+
+function handleForm(event) { event.preventDefault(); } 
+form.addEventListener('submit', handleForm);
+
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -11,5 +18,24 @@ function beginSeizure() {
     requestAnimationFrame(beginSeizure);
     document.querySelector("html").style.backgroundColor = getRandomColor();
     setTimeout(100);
+}
+
+function go() {
+    var roomID = document.getElementById("gameID").value;
+    console.log("Game ID: " + roomID);
+    if (buttonIndex == 0) {
+        joinRoom(roomID);
+    }
+    else if (buttonIndex == 1) {
+        createRoom(roomID);   
+    }
+}
+
+function joinRoom() {
+    
+}
+
+function createRoom() {
+    
 }
 
