68fab206c67785884b5b77d8322966860df3f5dd
Downs, Bert <bertkdowns@gmail.com>
2020-08-08 21:22:54 +1200

    doing the server!

---
+++ server/app.js
@@ -0,0 +1,53 @@
+"use strict"
+const WebSocket = require('ws');
+ 
+const wss = new WebSocket.Server({ port: 8080 });
+rooms = {};
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(jsondata) {// assuming data is always string
+    data = JSON.parse(jsondata);
+    switch (data.type) {
+        case "connect":
+            // connect to an existing room
+            if(rooms[data.id] != undefined){
+                ws.room = rooms[data.id]
+                console.log("User " +data.name+"joined room " + data.id );
+                ws.send(JSON.stringify({
+                    type:"connect",
+                    status:"success"
+                }))
+            } else{
+                ws.send(JSON.stringify({
+                    type:"connect",
+                    status:"fail"
+                }))
+            }
+            break;
+        case "create":
+            // creating a room if room id not taken
+            if(rooms[data.id] == undefined){
+                rooms[data.id] = {
+                    questions:data.questiondata,
+                    questionsShowing:{},// stores the key value pairs of questions currently showing and their answers
+                    answersShowing:{},// stores the key-value pairs of answers currently showing, that dont have a question showing as well!
+                }
+                ws.room = rooms[data.id]
+                console.log("User " +data.name+"joined room " + data.id );
+                ws.send(JSON.stringify({
+                    type:"connect",
+                    status:"success"
+                }))
+            } else{
+                ws.send(JSON.stringify({
+                    type:"create",
+                    status:"fail"
+                }))
+            }
+            break;
+        default:
+            break;
+    }
+  });
+  ws.on("close",()=>{
+  })
+});
